b7460d41ef1e5779ec0ef05c96896d36771cd7e0
Syed Pervaiz <syedaliakrampervaiz@gmail.com>
2018-09-22 15:19:12 -0500

    Working proto

--- bot.js
+++ bot.js
@@ -1,13 +1,12 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
-// Configure logger settings
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-// Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
    autorun: true
@@ -18,22 +17,28 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+    if(message=="!help"||message=="!")
+    {
+        bot.sendMessage({to: channelID,message: "Hello, Im Fornite Pro! Here's a list of commands. \n !tip"});
+    }
+    if(message=="!tip")
+    {
+        var data = null;
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+          if (this.readyState === 4) {
+            var obj = JSON.parse(this.responseText);
+            var category = parseInt(Math.random()*9)+1;
+            var tip = parseInt(Math.random()*obj.sections[category].content[0].elements.length);
+            bot.sendMessage({to: channelID,message: obj.sections[category].content[0].elements[tip].text});
+          }
+        });
+
+        xhr.open("GET", "http://fortnite.wikia.com/api/v1/Articles/AsSimpleJson?id=4609");
+        xhr.setRequestHeader("Cache-Control", "no-cache");
+        xhr.setRequestHeader("Postman-Token", "9873eadc-343d-4db7-84cd-bee49bbbdd78");
+        xhr.send(data);
+    }
+}); 
